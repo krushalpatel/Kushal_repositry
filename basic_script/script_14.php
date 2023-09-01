3078b4d7ac40638608b9778c3d1303a112ae5020
kushal masot <113751908+krushalpatel@users.noreply.github.com>
2023-09-01 17:36:58 +0530

    Add files via upload

---
+++ basic_script/script_14.php
@@ -0,0 +1,16 @@
+<!-- 14.Create a PHP program that uses echo to display the following text with proper formatting:
+    line1
+        line2
+            line3 -->
+ <?php
+    echo "<ul>\n";
+    echo "    line1\n";
+    echo "    <ul>\n";
+    echo "       line2\n";
+    echo "        <ul>\n";
+    echo "            line3\n";
+    echo "        </ul>\n";
+    echo "    </ul>\n";
+    echo "</ul>\n";
+?>
+
---
+++ basic_script/script_2.php
@@ -0,0 +1,8 @@
+<!-- . Create a PHP program that uses concatenation to display the following message:
+i. My name is John, and I am a PHP developer. -->
+
+<?php
+
+ echo "My name is " . "Kushal" . ", and I am a " . "PHP developer" . ".";
+
+?>
