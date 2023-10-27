274e1feeb2fabe4fa5f3fc2ef0a4aa131a675b9e
kushal masot <113751908+krushalpatel@users.noreply.github.com>
2023-10-27 18:14:26 +0530

    Add files via upload

---
+++ task_2/script_12.php
@@ -0,0 +1,9 @@
+<!-- Write a PHP script to concatenate two strings and display the combined result. -->
+<?php
+$string1 = "Kushal";
+$string2 = "Masot";
+
+$combined = $string1 . $string2;
+
+echo $combined;
+?>
---
+++ task_2/script_18.php
@@ -0,0 +1,17 @@
+<!-- Write a PHP script that generates a random number between 1 and 10 and lets the user
+guess it. -->
+
+<?php
+
+    $num = 4;
+    $rendom =rand(1,10);
+
+    echo "You enterd number : $num </br>";
+    echo "Random number : $rendom";
+    echo "</br>";
+    if($num==$rendom)
+        echo "Your number is match with rendom number.";
+    else 
+        echo "Your numbers is not match with rendom number.";
+
+?>
