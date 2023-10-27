2937597159b1badb7d35993412ba6782751760b8
kushal masot <113751908+krushalpatel@users.noreply.github.com>
2023-10-27 18:13:41 +0530

    Add files via upload

---
+++ task_1/script_2.php
@@ -0,0 +1,9 @@
+<!-- Create variables $name and $age, and use the echo statement to display a message like
+"My name is [Name] and I am [Age] years old." -->
+
+<?php
+$name = "Kushal Masot"; 
+$age = 21;       
+
+echo "My name is $name and I am $age years old.";
+?>
---
+++ task_1/script_3.php
@@ -0,0 +1,11 @@
+<!-- Calculate and display the sum of two variables $num1 and $num2 using the echo
+statement. -->
+
+<?php
+$num1 = 10; 
+$num2 = 20; 
+
+$sum = $num1 + $num2;
+
+echo "The sum of $num1 and $num2 is :- $sum";
+?>
